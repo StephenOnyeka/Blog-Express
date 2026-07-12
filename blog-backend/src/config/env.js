c0c03fc72ef4b352a13e1c21d3fe77d4bcc22db0
StephenOnyeka <ejeanobionyeka@gmail.com>
2026-07-12 15:27:24 +0100

    updated env file

--- blog-backend/src/config/env.js
+++ blog-backend/src/config/env.js
@@ -4,13 +4,13 @@ const { z } = require('zod');
 dotenv.config();
 
 const envSchema = z.object({
-  PORT: z.string().default('3000'),
+  PORT: z.string().default('8080'),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
   JWT_EXPIRES_IN: z.string().default('7d'),
   RESEND_API: z.string(),
-  FROM_EMAIL: z.string().email().default('no-reply@blog.com'),
+  FROM_EMAIL: z.string().email().default('onboarding@resend.dev'),
 });
 
 const _env = envSchema.safeParse(process.env);
