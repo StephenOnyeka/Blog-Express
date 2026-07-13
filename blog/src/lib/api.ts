364d0f7585cc347946bb1ecccbe3e41a1636e472
StephenOnyeka <ejeanobionyeka@gmail.com>
2026-07-13 21:39:58 +0100

    created a hook for handling fetch

--- blog/src/lib/api.ts
+++ blog/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base instance
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // proxy or direct
+  baseURL: 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json',
   },
